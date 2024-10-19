258a9b8fa8758a3d488c32f83b0001df64357d56
Luwaj <ntnluahttt2211014@student.ctuet.edu.vn>
2024-10-19 13:34:55 +0700

    lang que cua ngoc lua

---
+++ langqueweb/index.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="Logo.jpg" />
+        <meta name="author" content="Logo.jpg" />
+        <title >
+            Làng Quê Store
+        </title>
+        <meta property="og:image" content="Logo.jpg">
+        <link rel="icon" type="image/x-icon" href="Logo.jpg" />
+        <!-- Font Awesome icons (free version)--
+        
+            <!-- Bootstrap CSS -->
+            <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+            <!-- jQuery, Popper.js, and Bootstrap JS -->
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+      
+          
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top"><img src="img/bglogo.png"  width="200" height="150" alt=""></a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <!-- <li class="nav-item"><a class="nav-link" href="#about">Contact</a></li> -->
+                        <li class="nav-item"><a class="nav-link" href="pages/login.php">Giới Thiệu</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="./pages/trangchu.php">Trang chủ</a></li>
+                        <li class="nav-item"><a class="nav-link" href="./pages/sanpham.php">Sản phẩm</a></li>
+                        <li class="nav-item"><a class="nav-link" href="./pages/giohang.php">Giỏ Hàng</a></li>
+                        <!-- <form action="./pages/sanpham.html">
+                            <button type="submit" class="btn btn-warning">Xem sản phẩm</button>
+                          </form> -->
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            
+            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <div class="d-flex justify-content-center">
+                    <div class="text-center">
+                        <h1 class="mx-auto my-0 text-uppercase">LANGQUE</h1>
+                        <h2 class="text-white-50 mx-auto mt-2 mb-5">"Lưu giữ khoảnh khắc, tặng trọn yêu thương"</h2>
+                        <a class="btn btn-primary" href="./pages/gioithieu.php">Get Started</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- <div class="py-5">
+                <?php
+                    $allowed_pages = ['trangchu', 'lienhe', 'sanpham', 'gioithieu']; // Các trang được phép
+                    if(in_array($page, $allowed_pages)) {
+                        include ("pages/".$page.".php");
+                    } else {
+                        include ("pages/404.php"); // Nhúng trang 404 nếu trang không tồn tại
+                    }
+                    
+                ?>
+         </div> -->
+           
+
+
+        <!--main-->
+       
+        <!-- About-->
+        <!-- <section class="about-section text-center" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2 class="text-white mb-4">Built with Bootstrap 5</h2>
+                        <p class="text-white-50">
+                            Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
+                            <a href="https://startbootstrap.com/theme/grayscale/">the preview page.</a>
+                            The theme is open source, and you can use it for any purpose, personal or commercial.
+                        </p>
+                    </div>
+                </div>
+                <img class="img-fluid" src="assets/img/ipad.png" alt="..." />
+            </div>
+        </section> -->
+
+        <!-- Projects-->
+   <section class="projects-section bg-light" id="projects">
+            <div class="container px-4 px-lg-5">
+                <!-- Featured Project Row-->
+                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="img/ChieuDinhYen.jpg" alt="..." /></div>
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>Chiếu Định Yên</h4>
+                            <p class="text-black-50 mb-0">Chiếu Định Yên là một sản phẩm thủ công truyền thống nổi tiếng của làng Định Yên, huyện Lấp Vò, tỉnh Đồng Tháp. Nghề làm chiếu ở đây đã tồn tại từ hơn 100 năm, với kỹ thuật dệt chiếu độc đáo và tinh tế. Người thợ sử dụng những sợi lác tự nhiên, qua các công đoạn nhuộm màu và dệt thành những chiếc chiếu đa sắc với họa tiết phong phú, bền bỉ. Chiếu Định Yên không chỉ phục vụ cho nhu cầu sinh hoạt hàng ngày, mà còn là nét đẹp văn hóa, là minh chứng cho sự sáng tạo và khéo léo của người dân vùng sông nước Nam Bộ.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                   
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>Chiếu Cà Mau</h4>
+                            <p class="text-black-50 mb-0">Chiếu Cà Mau là một sản phẩm thủ công truyền thống nổi tiếng của vùng đất Cà Mau, miền Tây Nam Bộ. Được làm từ nguyên liệu tự nhiên là cây cói, chiếu Cà Mau mang nét đẹp mộc mạc, giản dị và bền bỉ. Người thợ dệt chiếu Cà Mau với kỹ thuật khéo léo và tỉ mỉ đã tạo nên những sản phẩm chiếu dệt chắc chắn, hoa văn đơn giản nhưng tinh tế, đáp ứng nhu cầu sử dụng hàng ngày của người dân địa phương cũng như du khách. Chiếu Cà Mau không chỉ là vật dụng thiết yếu mà còn chứa đựng giá trị văn hóa truyền thống của người Việt.</p>
+                        </div>
+                    </div>
+                     <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="img/ChieuCaMau.jpg" alt="..." /></div>
+                </div>
+                <!-- Project One Row-->
+                <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="img/thocamChauGiang.jpg" alt="..." /></div>
+                    <div class="col-lg-6">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 class="text-white">Làng dệt thổ cẩm Châu Giang</h4>
+                                    <p class="mb-0 text-white-50">Làng dệt thổ cẩm Châu Giang, nằm ở tỉnh An Giang, là một trong những làng nghề truyền thống lâu đời của người Chăm. Nổi tiếng với kỹ thuật dệt thổ cẩm tinh xảo, làng Châu Giang tạo ra những sản phẩm độc đáo với hoa văn rực rỡ và màu sắc tự nhiên. Sản phẩm thổ cẩm ở đây không chỉ mang tính thẩm mỹ cao mà còn gắn liền với văn hóa và tín ngưỡng của người Chăm.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Project Two Row-->
+                <div class="row gx-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="img/ttcnPhuocLong.jpg" alt="..." /></div>
+                    <div class="col-lg-6 order-lg-first">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-right">
+                                    <h4 class="text-white">Làng nghề tiểu thủ công nghệ Phước Long</h4>
+                                    <p class="mb-0 text-white-50">Làng nghề tiểu thủ công nghiệp Phước Long, thuộc huyện Giồng Trôm, tỉnh Bến Tre, nổi tiếng với các sản phẩm thủ công làm từ dừa. Với nguồn nguyên liệu dừa dồi dào tại địa phương, người dân Phước Long đã tạo ra nhiều sản phẩm đa dạng như thảm, túi xách, mũ, và các vật dụng trang trí từ xơ, gáo, và lá dừa. Làng nghề không chỉ bảo tồn các giá trị văn hóa truyền thống mà còn góp phần phát triển kinh tế địa phương, tạo công ăn việc làm và cải thiện đời sống người dân</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section> 
+        
+           
+        <!-- Signup-->
+        <!-- <section class="signup-section" id="signup">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-md-10 col-lg-8 mx-auto text-center">
+                        <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+                        <h2 class="text-white mb-5">Subscribe to receive updates!</h2>
+                         * * * * * * * * * * * * * * *-->
+                        <!-- * * SB Forms Contact Form * *-->
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- This form is pre-integrated with SB Forms.-->
+                        <!-- To make this form functional, sign up at-->
+                        <!-- https://startbootstrap.com/solution/contact-forms-->
+                        <!-- to get an API token!-->
+
+
+                       <!-- <form class="form-signup" id="contactForm" data-sb-form-api-token="API_TOKEN"> -->
+                            <!-- Email address input-->
+
+                            <!-- <div class="row input-group-newsletter">
+                                <div class="col"><input class="form-control" id="emailAddress" type="email" placeholder="Enter email address..." aria-label="Enter email address..." data-sb-validations="required,email" /></div>
+                                <div class="col-auto"><button class="btn btn-primary disabled" id="submitButton" type="submit">Notify Me!</button></div>
+                            </div>
+                            <div class="invalid-feedback mt-2" data-sb-feedback="emailAddress:required">An email is required.</div>
+                            <div class="invalid-feedback mt-2" data-sb-feedback="emailAddress:email">Email is not valid.</div> -->
+                            <!-- Submit success message-->
+                            <!---->
+                            <!-- This is what your users will see when the form-->
+                            <!-- has successfully submitted-->
+
+                            <!-- <div class="d-none" id="submitSuccessMessage">
+                                <div class="text-center mb-3 mt-2 text-white">
+                                    <div class="fw-bolder">Form submission successful!</div>
+                                    To activate this form, sign up at
+                                    <br />
+                                    <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                </div>
+                            </div> -->
+                            <!-- Submit error message-->
+                            <!---->
+                            <!-- This is what your users will see when there is-->
+                            <!-- an error submitting the form-->
+                            <!-- <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3 mt-2">Error sending message!</div></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section> -->
+        <!-- Contact-->
+        <!-- <section class="contact-section bg-black">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Address</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50">4923 Market Street, Orlando FL</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-envelope text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Email</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50"><a href="#!">hello@yourdomain.com</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Phone</h4>
+                                <hr class="my-4 mx-auto" />
+                                <div class="small text-black-50">+1 (555) 902-8832</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="social d-flex justify-content-center">
+                    <a class="mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                    <a class="mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                    <a class="mx-2" href="#!"><i class="fab fa-github"></i></a>
+                </div>
+            </div>
+        </section> -->
+        <!-- Footer-->
+        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Your Website 2023</div></footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+        
+    </body>
+</html>
---
+++ langqueweb/pages/gioithieu.php
@@ -0,0 +1,50 @@
+<div class="container px-4 px-lg-5">
+                <!-- Featured Project Row-->
+                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="img/ChieuDinhYen.jpg" alt="..." /></div>
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>Chiếu Định Yên</h4>
+                            <p class="text-black-50 mb-0">Chiếu Định Yên là một sản phẩm thủ công truyền thống nổi tiếng của làng Định Yên, huyện Lấp Vò, tỉnh Đồng Tháp. Nghề làm chiếu ở đây đã tồn tại từ hơn 100 năm, với kỹ thuật dệt chiếu độc đáo và tinh tế. Người thợ sử dụng những sợi lác tự nhiên, qua các công đoạn nhuộm màu và dệt thành những chiếc chiếu đa sắc với họa tiết phong phú, bền bỉ. Chiếu Định Yên không chỉ phục vụ cho nhu cầu sinh hoạt hàng ngày, mà còn là nét đẹp văn hóa, là minh chứng cho sự sáng tạo và khéo léo của người dân vùng sông nước Nam Bộ.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                   
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>Chiếu Cà Mau</h4>
+                            <p class="text-black-50 mb-0">Chiếu Cà Mau là một sản phẩm thủ công truyền thống nổi tiếng của vùng đất Cà Mau, miền Tây Nam Bộ. Được làm từ nguyên liệu tự nhiên là cây cói, chiếu Cà Mau mang nét đẹp mộc mạc, giản dị và bền bỉ. Người thợ dệt chiếu Cà Mau với kỹ thuật khéo léo và tỉ mỉ đã tạo nên những sản phẩm chiếu dệt chắc chắn, hoa văn đơn giản nhưng tinh tế, đáp ứng nhu cầu sử dụng hàng ngày của người dân địa phương cũng như du khách. Chiếu Cà Mau không chỉ là vật dụng thiết yếu mà còn chứa đựng giá trị văn hóa truyền thống của người Việt.</p>
+                        </div>
+                    </div>
+                     <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="img/ChieuCaMau.jpg" alt="..." /></div>
+                </div>
+                <!-- Project One Row-->
+                <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="img/thocamChauGiang.jpg" alt="..." /></div>
+                    <div class="col-lg-6">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 class="text-white">Làng dệt thổ cẩm Châu Giang</h4>
+                                    <p class="mb-0 text-white-50">Làng dệt thổ cẩm Châu Giang, nằm ở tỉnh An Giang, là một trong những làng nghề truyền thống lâu đời của người Chăm. Nổi tiếng với kỹ thuật dệt thổ cẩm tinh xảo, làng Châu Giang tạo ra những sản phẩm độc đáo với hoa văn rực rỡ và màu sắc tự nhiên. Sản phẩm thổ cẩm ở đây không chỉ mang tính thẩm mỹ cao mà còn gắn liền với văn hóa và tín ngưỡng của người Chăm.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Project Two Row-->
+                <div class="row gx-0 justify-content-center">
+                    <div class="col-lg-6"><img class="img-fluid" src="img/ttcnPhuocLong.jpg" alt="..." /></div>
+                    <div class="col-lg-6 order-lg-first">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-right">
+                                    <h4 class="text-white">Làng nghề tiểu thủ công nghệ Phước Long</h4>
+                                    <p class="mb-0 text-white-50">Làng nghề tiểu thủ công nghiệp Phước Long, thuộc huyện Giồng Trôm, tỉnh Bến Tre, nổi tiếng với các sản phẩm thủ công làm từ dừa. Với nguồn nguyên liệu dừa dồi dào tại địa phương, người dân Phước Long đã tạo ra nhiều sản phẩm đa dạng như thảm, túi xách, mũ, và các vật dụng trang trí từ xơ, gáo, và lá dừa. Làng nghề không chỉ bảo tồn các giá trị văn hóa truyền thống mà còn góp phần phát triển kinh tế địa phương, tạo công ăn việc làm và cải thiện đời sống người dân</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
